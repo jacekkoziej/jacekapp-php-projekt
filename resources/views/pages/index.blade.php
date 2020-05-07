27cc5875529af8a7012b1ac3d17772a56eb60fbf
Jacek <jacek.dominik.koziej@gmail.com>
2020-05-07 18:18:24 +0200

    stworzenie projektu laravel

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Laravel</title>
+    </head>
+    <body>
+    <h1>Witam w mojej aplikacji na zaliczenie przedmiotu</h1>
+    </body>
+</html>
