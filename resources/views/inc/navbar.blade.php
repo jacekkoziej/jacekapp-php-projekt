9dc120c44200ee9871180129c2ed553c55108720
Jacek <jacek.dominik.koziej@gmail.com>
2020-05-13 01:01:29 +0200

    uposledzony navbar

---
+++ resources/views/inc/navbar.blade.php
@@ -0,0 +1,13 @@
+<nav class="navbar navbar-expand navbar-dark bg-dark">
+    <a class="navbar-brand" href="/">Jacek App</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample09" aria-controls="navbarsExample09" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="icon-bar"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarsExample03">
+        <ul class="navbar-nav mr-auto">
+            <li><a href="/about">O mnie</a></li>
+            <li><a href="/services">Usługi</a></li>
+        </ul>
+    </div>
+</nav>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,12 +1,15 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ config('app.locale')}}">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Laravel</title>
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <title>{{config('app.name', 'jacekapp')}}</title>
 </head>
 <body>
-    @yield('content')
+    @include('inc.navbar')
+        <div class="container">
+            @yield('content')
+        </div>
 </body>
 </html>
