d2884e51abd54e0e6d261477b1ad2b45d446fbf5
Jacek <jacek.dominik.koziej@gmail.com>
2020-05-14 20:36:52 +0200

    zablokowanie możliwości usuwania postow przez niezalogowanych uzytkownikow

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -7,12 +7,16 @@
         {!! $post->body !!}
     </div>
     <hr>
-    <small>Dodano {{$post->created_at}} przez {{$posts->user->name}}</small>
+    <small>Dodano {{$post->created_at}} przez {{$post->user->name}}</small>
     <hr>
-    <a href="/posts/{{$post->id}}/edit" class="btn btn-dark">Edytuj</a>
-    {!! Form::open(['action'=>['PostController@destroy', $post->id], 'method'=> 'POST', 'class'=>'float-right' ]) !!}
-        {{Form::hidden('_method', 'DELETE')}}
-        {{Form::submit('Delete', ['class' => 'btn btn-danger'])}}
-    {!! Form::close() !!}
+    @if(!Auth::guest())
+        @if(Auth::user()->id == $post->user_id)
+            <a href="/posts/{{$post->id}}/edit" class="btn btn-dark">Edytuj</a>
+            {!! Form::open(['action'=>['PostController@destroy', $post->id], 'method'=> 'POST', 'class'=>'float-right' ]) !!}
+            {{Form::hidden('_method', 'DELETE')}}
+            {{Form::submit('Delete', ['class' => 'btn btn-danger'])}}
+            {!! Form::close() !!}
+        @endif
+    @endif
 @endsection
 
