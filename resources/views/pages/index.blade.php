66f1678b02407d6dd5e49cf9cd001cb3c0cd1fd2
Jacek <jacek.dominik.koziej@gmail.com>
2020-05-14 21:04:11 +0200

    git link

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -3,6 +3,8 @@
 @section('content')
     <div class="jumbotron text-center">
         <h1>Witam w mojej pierwszej aplikacji z wykorzystaniem Laravel!</h1>
-        <p>Strona ta została przygotowana na zaliczenie zajęć "Wstęp do programowania z wykorzystaniem frameworków PHP"</p>
+        <p>Aplikacja ta została przygotowana na zaliczenie zajęć "Wstęp do programowania z wykorzystaniem frameworków PHP"</p>
+        <p>Główna funkcja stworzonej aplikacji to strony z informacjami o mnie oraz blog z kontrolą dostępu użytkowników (może działać trochę jak forum).</p>
+        <a href="https://github.com/jacekkoziej/jacekapp-php-projekt">link do projektu w Github</a>
     </div>
 @endsection
