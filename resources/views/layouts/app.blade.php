17077b410ce8c7b8043ce14d135533a98de111c5
Jacek <jacek.dominik.koziej@gmail.com>
2020-05-14 19:41:02 +0200

    php artisan ui:auth - dodanie logowania i rejestracji oraz dostosowanie nowego paska nawigacji

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,36 +1,36 @@
-<!DOCTYPE html>
-<html lang="{{ app()->getLocale() }}">
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>{{ config('app.name', 'Jacek APP') }}</title>
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <script src="{{asset('js/ckeditor.js')}}"></script>
-    <script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
-    <script>tinymce.init({selector:'textarea'});</script>
-
 </head>
 <body>
-<div id="app">
-    @include('inc.navbar')
-    <div class="container">
-        @include('inc.messages')
-        @yield('content')
-    </div>
-</div>
+    <div id="app">
 
-<!-- Scripts -->
-<script src="{{ asset('js/app.js') }}"></script>
-<script>
-    ClassicEditor
-        .create( document.querySelector( '#editor' ) )
-        .catch( error => {
-            console.error( error );
-        } );
-</script>
+
+        <main class="py-4">
+            @include('inc.navbar')
+            <div class="container">
+                @yield('inc.messages')
+                @yield('content')
+            </div>
+
+        </main>
+    </div>
 </body>
 </html>
